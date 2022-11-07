ec02354bd14e7704eeff9f6f6a4207fe91d42035
MuhammedSinan12 <sinusinu1222@.com>
2022-11-07 14:53:00 +0530

    7thNov

--- flutter_assignment/lib/card.dart
+++ flutter_assignment/lib/card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Cardui extends StatelessWidget {
+  const Cardui({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
